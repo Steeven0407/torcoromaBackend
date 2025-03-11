eb5e34ae40db9cd9ba358cd5ef7f5e8ed2f8a25c
babayaga <steevenandre0407@gmail.com>
2025-03-10 20:11:26 -0500

    se añaden los endpoints de agente de pastoral

--- src/routes/gruposparroquiales.routes.js
+++ src/routes/gruposparroquiales.routes.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
-import { buscarGrupoParroquial, publicarGrupoParroquial } from '../controllers/gruposParroquiales.controllers.js'
+import { buscarGrupoParroquial, publicarGrupoParroquial, publicarAgentePastoral, editarAgentePastoral, buscarAgentePastoral } from '../controllers/gruposParroquiales.controllers.js'
 const router = Router()
 
 router.get('/buscarGrupoParroquial', buscarGrupoParroquial);
 router.post('/publicarGrupoParroquial', publicarGrupoParroquial);
+router.post('/publicarAgentePastoral', publicarAgentePastoral);
+router.post('/editarAgentePastoral', editarAgentePastoral);
+router.get('/buscarAgentePastoral', buscarAgentePastoral);
 
 export default router
